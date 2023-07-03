bdf1c19d9e4d88d6f46c111ffed9213df8a74d71
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-03 13:53:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 
-col1, col2, col3:
+col1, col2, col3 = st.columns([0.2, 5, 0.2])
 with col2:
   introduction_text = '''
     <p class="introduction_text" style="margin-top: -1.25em; margin-bottom: 1.25em; text-align: justify;"><span style="color: #25476A; background-color: rgba(3, 169, 244, 0.2); border-radius: 0.375em; padding-left: 0.75em; padding-right: 0.75em; padding-top: 0.5em; padding-bottom: 0.5em; font-family: sans-serif; font-size: 1em; font-weight: bold; display: block; width: 100%; border: 0.1875em solid #25476A;">Welcome to the Paydar wargame scenario analysis application, empowering you to make informed decisions regarding the current and future financial performance of target companies based on manual and scenario-based financial statement and credit rating analyses.</span></p>
