13926b9bc80a4eb91a1a3d5f0ea5d69f0095c341
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-03 13:37:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 import streamlit as st
+
+
+
+hide_st_style = """
+                <style>
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                .viewerBadge_link__1S137 {display: none !important;}
+                </style>
+                """
+st.markdown(hide_st_style, unsafe_allow_html=True)
