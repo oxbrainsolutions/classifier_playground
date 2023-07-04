5a98cbfef4918a5965e9a9dec5af44bd31633f94
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-04 08:15:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -21,18 +21,18 @@ with col2:
   st.markdown(header_media_query + header_text, unsafe_allow_html=True)
 
 with st.sidebar:
-    subheader_text = '''
-    <p class="subheader_text" style="margin-top: -1.25em; margin-bottom: 1.25em; text-align: center;"><span style="color: #FAFAFA; font-family: sans-serif; font-size: 1em; ">Create a Dataset</span></p>
+  subheader_text = '''
+  <p class="subheader_text" style="margin-top: -1.25em; margin-bottom: 1.25em; text-align: center;"><span style="color: #FAFAFA; font-family: sans-serif; font-size: 1em; ">Create a Dataset</span></p>
   '''
 
   subheader_media_query = '''
-      <style>
-      @media (max-width: 1024px) {
-          p.subheader_text {
-            font-size: 0.6em;
-          }
-      }
-      </style>
+    <style>
+    @media (max-width: 1024px) {
+        p.subheader_text {
+          font-size: 0.6em;
+        }
+    }
+    </style>
   '''
   st.markdown(subheader_media_query + subheader_text, unsafe_allow_html=True)
   
