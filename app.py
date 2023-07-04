4619d98125b3843f25e43e74c8b67b658928f603
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-04 09:06:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ with st.sidebar:
   st.markdown(subheader_media_query + subheader_text, unsafe_allow_html=True)
 
 
-  text = '<p class="text" style="margin-top: -3em; margin-bottom: -3em;"><span style="font-family:sans-serif; color:#FAFAFA; font-size: 0.8em; ">Data Type</span></p>'
+  text = '<p class="text" style="margin-top: -3em; margin-bottom: -6em;"><span style="font-family:sans-serif; color:#FAFAFA; font-size: 0.8em; ">Data Type</span></p>'
   text_media_query1 = '''
   <style>
   @media (max-width: 1024px) {
