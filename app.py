c3f769ea2e541047ec29b29dfb7ac476c2b393f1
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-04 08:40:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,76 @@ from utils.functions import generate_data
 
 st.set_page_config(page_title="Classifier Playground", page_icon="", layout="wide")
 
+st.markdown(
+    """
+    <style>
+     div.stButton > button:first-child {
+        background-color: #25476A;
+        color: #FAFAFA;
+        border-color: #FAFAFA;
+        border-width: 3px;
+        width: 5.4em;
+        height: 1.8em;
+        margin-top: 1.5em;
+    }
+    div.stButton > button:hover {
+        background-color: rgba(111, 114, 222, 0.6);
+        color: #25476A;
+        border-color: #25476A;
+    }
+    @media (max-width: 1024px) {
+    div.stButton > button {
+        width: 100% !important;
+        height: 10em !important;
+        margin-top: -3em;
+        }
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+  )
+st.markdown("""
+  <style>
+  /* The input itself */
+  div[data-baseweb="select"] > div,
+  input[type=number] {
+  margin-top: 0;
+  color: #25476A;
+  background-color: rgba(3, 169, 244, 0.2);
+  border: 0.1875em solid #25476A;
+  font-size: 1em;
+  font-weight: bold;
+  }
+  /* Hover effect */
+  div[data-baseweb="select"] > div:hover,
+  input[type=number]:hover {
+  background-color: rgba(111, 114, 222, 0.4);
+  }
+  span.st-bj.st-cf.st-ce.st-f3.st-f4.st-af {
+  font-size: 1em;
+  font-weight: bold;
+  }
+  @media (max-width: 1024px) {
+    span.st-bj.st-cf.st-ce.st-f3.st-f4.st-af {
+    font-size: 0.8em;
+    }
+  }
+  
+  /* Media query for small screens */
+  @media (max-width: 1024px) {
+  div[data-baseweb="select"] > div,
+  input[type=number] {
+    font-size: 0.8em;
+    height: 3em;
+  }
+  .stMultiSelect [data-baseweb="select"] > div,
+  .stMultiSelect [data-baseweb="tag"] {
+    height: auto !important;
+  }
+  }
+  </style>
+  """, unsafe_allow_html=True)
+
 col1, col2, col3 = st.columns([0.2, 5, 0.2])
 with col2:
   header_text = '''
@@ -36,6 +106,33 @@ with st.sidebar:
     </style>
   '''
   st.markdown(subheader_media_query + subheader_text, unsafe_allow_html=True)
+
+
+  text = '<p class="text" style="margin-bottom: 0em;"><span style="font-family:sans-serif; color:#25476A; font-size: 0.85em; font-weight: bold;">Data Type</span></p>'
+  text_media_query1 = '''
+  <style>
+  @media (max-width: 1024px) {
+      p.text {
+          font-size: 3.5em;
+      }
+  }
+  </style>
+  '''
+  st.markdown(text_media_query1 + text, unsafe_allow_html=True)
+  data_type_options = ["Blobs", "Circles", "Spirals"]
+  st.selectbox(label="", label_visibility="collapsed", options=data_type_options,
+               format_func=lambda x: "Select Data Type" if x == "" else x, key="user_data_type", on_change=change_callback1)
+
+
+
+
+
+
+
+
+
+
+
   
   st.selectbox(
     "How would you like to be contacted?",
