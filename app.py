b0b1f2241a100b32965c1871be30ae071ead9240
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-05 14:01:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ header = """
             flex-direction: column;
             justify-content: center;
             align-items: center;
-            padding: 0.4em;  /* Adjust padding for smaller screens */
+            padding: 1em;  /* Adjust padding for smaller screens */
        }}
 
         .middle-column {{
