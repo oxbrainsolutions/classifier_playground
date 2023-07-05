dc281180fedd97666f32305120ff7d8bc8149395
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-05 14:44:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -182,7 +182,7 @@ st.markdown("""
 col1, col2, col3 = st.columns([0.2, 5, 0.2])
 with col2:
   header_text = '''
-    <p class="header_text" style="margin-top: 4em; margin-bottom: 1.25em; text-align: center;"><span style="color: #FAFAFA; font-family: Liberation Sans, sans-serif; font-size: 1.8em; ">Supervised Machine Learning Classification</span></p>
+    <p class="header_text" style="margin-top: 4em; margin-bottom: 1.25em; text-align: center;"><span style="color: #FAFAFA; font-family: "Liberation Sans", sans-serif; font-size: 1.8em; ">Supervised Machine Learning Classification</span></p>
   '''
 
   header_media_query = '''
