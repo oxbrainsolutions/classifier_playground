eb920cf0b328a56264a4f3801d563ebe651ddd23
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-05 15:50:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -215,7 +215,7 @@ with st.sidebar:
 
 
 
-  text = '<p class="text" style="margin-top: --2.4em; margin-bottom: -6em;"><span style="font-family:sans-serif; color:#FAFAFA; font-size: 0.8em; ">Data Type</span></p>'
+  text = '<p class="text" style="margin-top: -2.4em; margin-bottom: -6em;"><span style="font-family:sans-serif; color:#FAFAFA; font-size: 0.8em; ">Data Type</span></p>'
   text_media_query1 = '''
   <style>
   @media (max-width: 1024px) {
