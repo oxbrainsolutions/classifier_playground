b91ca4d8264cd45135df1175354db9595e65557d
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-05 16:44:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -260,16 +260,16 @@ with st.sidebar:
 
   text = '<p class="text" style="margin-top: 0em; margin-bottom: 0em;"><span style="font-family:sans-serif; color:#FAFAFA; font-size: 0.8em; ">Sample Size</span></p>'
   st.markdown(text_media_query1 + text, unsafe_allow_html=True)
-  n_samples = st.number_input(label="", label_visibility="collapsed", min_value=50, max_value=1000, step=10, value=300, key="key1")
+  user_n_samples = st.number_input(label="", label_visibility="collapsed", min_value=50, max_value=1000, step=10, value=300, key="key2")
   text = '<p class="text" style="margin-top: 0em; margin-bottom: 0em;"><span style="font-family:sans-serif; color:#FAFAFA; font-size: 0.8em; ">Training Data Noise</span></p>'
   st.markdown(text_media_query1 + text, unsafe_allow_html=True)
-  train_noise = st.slider(label="", label_visibility="collapsed", min_value=0.01, max_value=1.0, step=0.005, value=0.2, key="key2")
+  user_train_noise = st.slider(label="", label_visibility="collapsed", min_value=0.01, max_value=1.0, step=0.005, value=0.2, key="key2")
   text = '<p class="text" style="margin-top: 0em; margin-bottom: 0em;"><span style="font-family:sans-serif; color:#FAFAFA; font-size: 0.8em; ">Testing Data Noise</span></p>'
   st.markdown(text_media_query1 + text, unsafe_allow_html=True)
-  test_noise = st.slider(label="", label_visibility="collapsed", min_value=0.01, max_value=1.0, step=0.005, value=train_noise, key="key3")
+  user_test_noise = st.slider(label="", label_visibility="collapsed", min_value=0.01, max_value=1.0, step=0.005, value=train_noise, key="key2")
   submit_button = st.button("Run", key="key4")
-
-  x_train, y_train, x_test, y_test = generate_data(dataset, n_samples, train_noise, test_noise, n_classes)
+  if submit_button:
+    x_train, y_train, x_test, y_test = generate_data(user_data_type, user_n_samples, user_train_noise, user_test_noise, n_classes=2)
 
 
 
