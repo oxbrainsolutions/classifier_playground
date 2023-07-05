da28abe765bc6088999f7bbbbe2cc540a30eed23
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-05 16:00:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ line_media_query = '''
     <style>
     @media (max-width: 1024px) {
         .line1 {
-            padding: 0.5em;
+            padding: 0.3em;
             margin-top: -2em; 
             margin-bottom: 8em;
         }
