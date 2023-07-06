c78ccc90b9d17661b6d41a4cae40a8c06a50080d
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-05 17:10:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ st.markdown(
         border-color: #FAFAFA;
         border-width: 0.15em;
         width: 100%;
-        height: 0.6em;
+        height: 0.5em;
         margin-top: 0em;
     }
     div.stButton > button:hover {
