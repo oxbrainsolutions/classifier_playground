b4c42b02fd1c8383e9939fd0e02bbc8a1e6592d5
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-06 13:30:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -268,7 +268,7 @@ with st.sidebar:
   st.session_state.user_train_noise = st.slider(label="", label_visibility="collapsed", min_value=0.01, max_value=1.0, step=0.005, value=0.2, key="key3")
   text = '<p class="text" style="margin-top: 0em; margin-bottom: 0em;"><span style="font-family:sans-serif; color:#FAFAFA; font-size: 0.8em; ">Testing Data Noise</span></p>'
   st.markdown(text_media_query1 + text, unsafe_allow_html=True)
-  st.session_state.user_test_noise = st.slider(label="", label_visibility="collapsed", min_value=0.01, max_value=1.0, step=0.005, value=user_train_noise, key="key4")
+  st.session_state.user_test_noise = st.slider(label="", label_visibility="collapsed", min_value=0.01, max_value=1.0, step=0.005, value=st.session_state.user_train_noise, key="key4")
   submit_button1 = st.button("Generate Dataset", key="key5")
   subheader_text_field1 = st.empty()
   line_field = st.empty()
