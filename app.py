af4485ed9956747cffa3e615b26b2db7f3df091b
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-06 10:54:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ st.markdown("""
   color: #FAFAFA;
   background-color: #4F5254;
   border: 0.25em solid #002147;
-  font-size: 0.7em;
+  font-size: 0.8em;
   font-family: sans-serif;
   height: 3em;
   }
