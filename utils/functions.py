887b97d7fbe45279c39e2f5009be90b9465198a2
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-05 17:08:12 -0700

    Update functions.py

--- utils/functions.py
+++ utils/functions.py
@@ -63,7 +63,7 @@ def plot_scatter(x_train, y_train, x_test, y_test):
             size=10,
             color=y_train,
             colorscale=["#FCBC24", "#008080"],
-            line=dict(color="#FAFAFA width=2),
+            line=dict(color="#FAFAFA", width=2),
         ),
     )
 
