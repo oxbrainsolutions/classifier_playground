ec23e320b0b6c7a5547ae3aac88c4546a9b8184d
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-06 12:42:18 -0700

    Update functions.py

--- utils/functions.py
+++ utils/functions.py
@@ -89,6 +89,6 @@ def plot_scatter(x_train, y_train, x_test, y_test):
 
     fig.update_xaxes(showline=True, showgrid=False, zeroline=False, linecolor = '#FAFAFA', linewidth = 2.5, mirror = True)
     fig.update_yaxes(showline=True, showgrid=False, zeroline=False, linecolor = '#FAFAFA', linewidth = 2.5, mirror = True)
-    fig.update_layout(height=700, width = 500, margin=dict(l=0, r=10, b=0, t=10), legend=dict(orientation="h", yanchor="top", y=1, xanchor="left", x=1))
+    fig.update_layout(height=700, width = 500, margin=dict(l=0, r=10, b=0, t=10), legend=dict(orientation="h", yanchor="top", y=1, xanchor="right", x=1))
     return fig
 
