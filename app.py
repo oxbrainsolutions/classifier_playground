7fec29d4a77a57c7cb5342266cbde8f7bb27b7b9
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-07 13:20:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -368,11 +368,11 @@ col1, col2, col3 = st.columns([1, 2, 1])
 with col1:
     if st.session_state.submit_confirm2 == True:
         create_gauge(num_value=np.round(train_accuracy, 2), label="Train\nAccuracy", key="key_gauge1")
-        text = '<p class="text" style="margin-top: -4em; margin-bottom: 0em; text-align: center;"><span style="font-family:sans-serif; color:#FAFAFA; font-size: 2em; ">{}</span></p>'.format(convert_rating(train_accuracy))
-          text_media_query2 = '''
+        text = '<p class="text2" style="margin-top: -4em; margin-bottom: 0em; text-align: center;"><span style="font-family:sans-serif; color:#FAFAFA; font-size: 2em; ">{}</span></p>'.format(convert_rating(train_accuracy))
+        text_media_query2 = '''
           <style>
           @media (max-width: 1024px) {
-              p.text {
+              p.text2 {
                   font-size: 3em;
               }
           }
