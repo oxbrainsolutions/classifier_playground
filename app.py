09ac656759e9560666cbfce6717b510f94090cf1
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-07 10:12:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -235,9 +235,7 @@ st.markdown("""
 
 
 with st.sidebar:
-      subheader_text1 = '''
-  <p class="subheader_text" style="margin-top: 0em; margin-bottom: 0em; text-align: justify;"><span style="color: #FAFAFA; font-family: sans-serif; font-size: 1em; ">Create a Dataset</span></p>
-  '''
+      subheader_text1 = '''<p class="subheader_text" style="margin-top: 0em; margin-bottom: 0em; text-align: justify;"><span style="color: #FAFAFA; font-family: sans-serif; font-size: 1em; ">Create a Dataset</span></p>'''
 
   subheader_media_query = '''
     <style>
