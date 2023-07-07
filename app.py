6c18bd2b1443a50937724fa0a413cfe3c306d823
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-07 13:17:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -368,7 +368,7 @@ col1, col2, col3 = st.columns([1, 2, 1])
 with col1:
     if st.session_state.submit_confirm2 == True:
         create_gauge(num_value=np.round(train_accuracy, 2), label="Train\nAccuracy", key="key_gauge1")
-        text = '<p class="text" style="margin-top: 0em; margin-bottom: 0em;"><span style="font-family:sans-serif; color:#FAFAFA; font-size: 0.8em; ">{}</span></p>'.format(convert_rating(train_accuracy))
+        text = '<p class="text" style="margin-top: 0em; margin-bottom: 0em; text-align: center;"><span style="font-family:sans-serif; color:#FAFAFA; font-size: 1.2em; ">{}</span></p>'.format(convert_rating(train_accuracy))
         st.markdown(text_media_query1 + text, unsafe_allow_html=True)
         create_gauge(num_value=np.round(train_f1, 2), label="Train\nF1 Score", key="key_gauge2")
 with col3:
