36a6356479ad245e5953ebdee7185bb366564cc7
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-07 15:10:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pathlib
 import base64
 import time
-from utils.functions import generate_data, plot_scatter, plot_scatter_decision_boundary, create_gauge, convert_rating, add_polynomial_features, train_model, lr_param_selector, nb_param_selector
+from utils.functions import generate_data, plot_scatter, plot_scatter_decision_boundary, create_gauge, convert_rating, add_polynomial_features, train_model, lr_param_selector, nb_param_selector, knn_param_selector
 
 
 st.set_page_config(page_title="Classifier Playground", page_icon="", layout="wide")
@@ -356,6 +356,10 @@ if st.session_state.submit_confirm1 == True:
           model = lr_param_selector()
       if st.session_state.user_model == "Naive Bayes":
         model = nb_param_selector()
+    
+
+      if st.session_state.user_model == "K Nearest Neighbor":
+          model = knn_param_selector
       
       if st.session_state.user_model != "":
           text = '<p class="text" style="margin-top: 0em; margin-bottom: 0em;"><span style="font-family:sans-serif; color:#FAFAFA; font-size: 0.8em; ">Polynomial Features Degree</span></p>'
