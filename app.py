45d3178f4d5536608535a759a090c6ae4990488a
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-07 11:26:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -357,10 +357,8 @@ if st.session_state.submit_confirm1 == True:
           if submit_button2:
               st.session_state.submit_confirm2 = True
               st.session_state.x_train_out, st.session_state.x_test_out = add_polynomial_features(st.session_state.x_train_out, st.session_state.x_test_out, user_poly_degree)
-              model, train_accuracy, train_f1, test_accuracy, test_f1, duration = train_model(model, st.session_state.x_train_out, st.session_state.y_train_out, st.session_state.x_test_out, st.session_state.y_test_out)
               try:
                   model, train_accuracy, train_f1, test_accuracy, test_f1, duration = train_model(model, st.session_state.x_train_out, st.session_state.y_train_out, st.session_state.x_test_out, st.session_state.y_test_out)
-                  st.write(train_accuracy, test_accuracy)
               except:
                   with st.sidebar:
                       st.error("**Error**: complete selection.")
