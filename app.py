40ff1a6d365e0e457f9baca9f21b6267fe566bf0
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-07 11:45:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -359,8 +359,6 @@ if st.session_state.submit_confirm1 == True:
               try:
                   model, train_accuracy, train_f1, test_accuracy, test_f1, duration = train_model(model, st.session_state.x_train_out, st.session_state.y_train_out, st.session_state.x_test_out, st.session_state.y_test_out
                   st.session_state.submit_confirm2 = True
-
-              
               except:
                   with st.sidebar:
                       st.error("**Error**: complete selection.")
