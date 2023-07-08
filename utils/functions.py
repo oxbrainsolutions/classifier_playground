3511fc029bf6507f5218c935a2677a62fd131f55
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-07 17:52:05 -0700

    Update functions.py

--- utils/functions.py
+++ utils/functions.py
@@ -13,6 +13,7 @@ from sklearn.metrics import accuracy_score, f1_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
@@ -347,7 +348,10 @@ def nb_param_selector():
 def lda_param_selector():
     model = LinearDiscriminantAnalysis()
     return model
-  
+
+def qda_param_selector():
+    model = QuadraticDiscriminantAnalysis()
+    return model
 
 def knn_param_selector():
     text = '<p class="text" style="margin-top: 0em; margin-bottom: 0em;"><span style="font-family:sans-serif; color:#FAFAFA; font-size: 0.8em; ">Number of Neighbors</span></p>'
