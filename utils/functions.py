51334041b82f69d45e27daf8fea27c2a145ad136
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-07 18:50:29 -0700

    Update functions.py

--- utils/functions.py
+++ utils/functions.py
@@ -116,7 +116,7 @@ def plot_scatter(x_train, y_train, x_test, y_test):
     fig.update_xaxes(showticklabels=False, showline=True, showgrid=False, zeroline=False, linecolor = '#FAFAFA', linewidth = 2.5, mirror = True)
     fig.update_yaxes(showticklabels=False, showline=True, showgrid=False, zeroline=False, linecolor = '#FAFAFA', linewidth = 2.5, mirror = True)
     fig.update_layout(autosize=True, height=500, width = 500, margin=dict(l=5, r=10, b=20, t=10), legend=dict(font=dict(size=14, family="sans-serif", color="#FAFAFA"), bgcolor="rgba(0, 0, 0, 0)", orientation="h", yref="paper", yanchor="bottom", y=0, xref="paper", xanchor="right", x=1))
-    fig.add_annotation(dict(font=dict(color="#FAFAFA", size=16, family="sans-serif"), yref="paper", yanchor="top", y=0, xref="paper", xanchor="left", x=0.5, showarrow=False, text="X1"))
+    fig.add_annotation(dict(font=dict(color="#FAFAFA", size=16, family="sans-serif"), yref="paper", yanchor="top", y=-0.4, xref="paper", xanchor="left", x=0.5, showarrow=False, text="X1"))
     return fig
 
 def plot_scatter_decision_boundary(label, model, x_train, y_train, x_test, y_test):
