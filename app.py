de9db1def5cd79ba4427052c891492dbb5cbdc91
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-10 14:08:32 -0700

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ st.markdown("""
 st.markdown("""
   <style>
     div.button.css-1l7fe1m.e1ewe7hr3 > button:first-child {
-        color: orange;
+        background-color: orange;
     }
     }
   </style>
