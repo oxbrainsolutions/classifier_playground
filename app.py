4e1066aaef9fc4d9f1f03c8e2d38c5ec1d39a3b9
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-11 15:51:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ st.markdown("""
     background-color: rgb(203, 175, 175) !important;
     }
     div[data-modal-container='true'][key='Modal1'] > div > div:nth-child(2) > div {
-        margin-left: 48em !important;
+        margin-left: 10em !important;
         max-width: 2em !important;
     }
     @media (max-width: 1024px) {
