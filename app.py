2c3808fdd240d426681ea6fdb38a04e1abdd1f73
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-11 16:00:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -374,7 +374,7 @@ if submit_button1:
 
 if modal.is_open():
     with modal.container():
-        error_text1 = '''<p class="error_text1" style="margin-top: 3em; margin-bottom: 5em; text-align: justify;"><span style="color: #850101; font-family: sans-serif; font-size: 1em; font-weight: bold;">Error blah blah blah</span></p>'''
+        error_text1 = '''<p class="error_text1" style="margin-top: 3em; margin-bottom: 1em; text-align: justify;"><span style="color: #850101; font-family: sans-serif; font-size: 1em; font-weight: bold;">Error blah blah blah</span></p>'''
         error_media_query1 = '''
         <style>
         @media (max-width: 1024px) {
@@ -385,6 +385,17 @@ if modal.is_open():
         </style>
         '''
         st.markdown(error_media_query1 + error_text1 , unsafe_allow_html=True)
+        error_text2 = '''<p class="error_text2" style="margin-top: 1em; margin-bottom: 1em; text-align: justify;"><span style="color: #850101; font-family: sans-serif; font-size: 1em; font-weight: bold;">Error blah blah blah</span></p>'''
+        error_media_query2 = '''
+        <style>
+        @media (max-width: 1024px) {
+            p.error_text2 {
+              font-size: 4em;
+            }
+        }
+        </style>
+        '''
+        st.markdown(error_media_query2 + error_text2 , unsafe_allow_html=True)
 
 if st.session_state.submit_confirm1 == True:
     with st.sidebar:
