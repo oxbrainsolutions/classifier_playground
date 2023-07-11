793f20c2bdcf7bb9a3d47835ac70093486af8650
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-11 13:54:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -338,7 +338,7 @@ if open_modal:
 
 if modal.is_open():
     with modal.container():
-        subheader_text1 = '''<p class="subheader_text" style="margin-top: 0em; margin-bottom: 0em; text-align: justify;"><span style="color: #008080; font-family: sans-serif; font-size: 1em; ">Create a Dataset</span></p>'''
+        subheader_text1 = '''<p class="subheader_text" style="margin-top: -3em; margin-bottom: 0em; text-align: justify;"><span style="color: #008080; font-family: sans-serif; font-size: 1em; ">Create a Dataset</span></p>'''
         subheader_media_query = '''
         <style>
         @media (max-width: 1024px) {
