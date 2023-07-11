d4626a4166dad61eb49ba37c5ed9ce450e68b397
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-11 12:27:52 -0700

    Update app.py

--- app.py
+++ app.py
@@ -325,21 +325,23 @@ if open_modal:
     modal.open()
 
 if modal.is_open():
-    with modal.container():
-        st.write("Text goes here")
-
-        html_string = '''
-        <h1>HTML string in RED</h1>
-
-        <script language="javascript">
-          document.querySelector("h1").style.color = "red";
-        </script>
-        '''
-        components.html(html_string)
-
-        st.write("Some fancy text")
-        value = st.checkbox("Check me")
-        st.write(f"Checkbox checked: {value}")
+    gr = st.columns(3):
+    with gr[2]:
+        with modal.container():
+            st.write("Text goes here")
+    
+            html_string = '''
+            <h1>HTML string in RED</h1>
+    
+            <script language="javascript">
+              document.querySelector("h1").style.color = "red";
+            </script>
+            '''
+            components.html(html_string)
+    
+            st.write("Some fancy text")
+            value = st.checkbox("Check me")
+            st.write(f"Checkbox checked: {value}")
 
     
 
