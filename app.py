448793cd5321dd6390991cf8b0a61bb8325c23a6
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-11 14:29:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -283,7 +283,7 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 with st.sidebar:
-    subheader_text1 = '''<p class="subheader_text" style="margin-top: 3em; margin-bottom: 0em; text-align: justify;"><span style="color: #FAFAFA; font-family: sans-serif; font-size: 1em; ">Error</span></p>'''
+    subheader_text1 = '''<p class="subheader_text" style="margin-top: 0em; margin-bottom: 0em; text-align: justify;"><span style="color: #FAFAFA; font-family: sans-serif; font-size: 1em; ">Create a Dataset</span></p>'''
     subheader_media_query = '''
     <style>
     @media (max-width: 1024px) {
@@ -335,7 +335,7 @@ if open_modal:
 
 if modal.is_open():
     with modal.container():
-        subheader_text1 = '''<p class="subheader_text" style="margin-top: 0em; margin-bottom: 0em; text-align: justify;"><span style="color: #008080; font-family: sans-serif; font-size: 1em; ">Create a Dataset</span></p>'''
+        subheader_text1 = '''<p class="subheader_text" style="margin-top: 3em; margin-bottom: 0em; text-align: justify;"><span style="color: #008080; font-family: sans-serif; font-size: 1em; ">Error</span></p>'''
         subheader_media_query = '''
         <style>
         @media (max-width: 1024px) {
