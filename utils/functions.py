dc04d9255cf68af5eca993a9235435ed2f7c3da6
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-11 18:07:50 -0700

    Update functions.py

--- utils/functions.py
+++ utils/functions.py
@@ -121,7 +121,7 @@ def plot_scatter(x_train, y_train, x_test, y_test):
     fig.add_annotation(dict(font=dict(color="#FAFAFA", size=16, family="sans-serif"), yref="paper", yanchor="top", y=-0.04, xref="paper", xanchor="left", x=0.5, showarrow=False, text="X1"))
     return fig
 
-def plot_scatter_decision_boundary(label, model, x_train, y_train, x_test, y_test):
+def plot_scatter_decision_boundary(model, x_train, y_train, x_test, y_test):
     d = x_train.shape[1]
     x_min, x_max = x_train[:, 0].min() - 1, x_train[:, 0].max() + 1
     y_min, y_max = x_train[:, 1].min() - 1, x_train[:, 1].max() + 1
@@ -192,7 +192,6 @@ def plot_scatter_decision_boundary(label, model, x_train, y_train, x_test, y_tes
     fig.update_yaxes(showticklabels=False, showline=True, showgrid=False, zeroline=False, linecolor = '#FAFAFA', linewidth = 2.5, mirror = True)
     fig.update_layout(autosize=True, height=500, width = 500, margin=dict(l=5, r=10, b=35, t=10), legend=dict(font=dict(size=14, family="sans-serif", color="#FAFAFA"), bgcolor="rgba(0, 0, 0, 0)", orientation="h", yref="paper", yanchor="bottom", y=0, xref="paper", xanchor="right", x=1))
     fig.add_annotation(dict(font=dict(color="#FAFAFA", size=16, family="sans-serif"), yref="paper", yanchor="top", y=-0.04, xref="paper", xanchor="left", x=0.5, showarrow=False, text="X1"))
-    fig.add_annotation(dict(font=dict(color="#FCBC24", size=25, family="sans-serif"), yref="paper", yanchor="top", y=0.98, xref="paper", xanchor="left", x=0.02, showarrow=False, text="{}".format(label)))
     return fig
 
 color1 = "#5007E3"
