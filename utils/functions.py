3ce6edeaae6fffc86fbc037dad5d797d1ece7eb3
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-07-27 10:36:53 -0700

    Update functions.py

--- utils/functions.py
+++ utils/functions.py
@@ -36,7 +36,6 @@ def change_callback3():
       st.session_state.modal2.close() 
 
 
-#@st.cache_data(suppress_st_warning=True, allow_output_mutation=True, show_spinner=False)
 def generate_data(dataset, n_samples, train_noise, test_noise, n_classes):
     if dataset == "Spirals":
         x_train, y_train = make_moons(n_samples=n_samples, noise=train_noise)
